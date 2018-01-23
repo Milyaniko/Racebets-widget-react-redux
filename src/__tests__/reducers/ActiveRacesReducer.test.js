15de179e0b85805943a22d0ea5456a9dd516eb38
Nikolay Milya <Milyaniko@gmail.com>
2018-01-23 19:10:10 +0700

    test is updated

--- src/__tests__/reducers/ActiveRacesReducer.test.js
+++ src/__tests__/reducers/ActiveRacesReducer.test.js
@@ -1,8 +1,8 @@
-import raceReducer from '../../app/reducers/ActiveRacesReducer';
+import raceActive from '../../app/reducers/ActiveRacesReducer';
 
 describe('Request Reducer', () => {
     it('has a inintial state', () => {
-        expect(raceReducer(undefined, { type: 'unexpected' })).toEqual({
+        expect(raceActive(undefined, { type: 'unexpected' })).toEqual({
             data: [],
             hasError: false,
             errorMessage: null
